6b5ed1c991517b222d6ef016886a1b4e68cced6f
Pawko <aleksander.pawlak@op.pl>
2025-01-14 12:08:36 +0100

    Correct class names in js file

--- storybook/js/storybook.js
+++ storybook/js/storybook.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuItems = document.querySelectorAll('.navbar__menu-item');
+    const menuItems = document.querySelectorAll('.navbar-link__menu-item');
 
     menuItems.forEach(item => {
         item.addEventListener('click', () => {
-            const icon = item.querySelector('.navbar__menu-icon');
+            const icon = item.querySelector('.navbar-link__menu-icon');
             if(icon) {
-                icon.classList.toggle('navbar__menu-icon--rotate')
+                icon.classList.toggle('navbar-link__menu-icon--rotate');
             }
-        })
-    })
-})
+        });
+    });
+});
